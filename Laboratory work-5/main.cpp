755caec36866fa965b625dc2b981fd157f76720b
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-08-15 19:53:07 +0300

    Update main.cpp

--- Laboratory work-5/main.cpp
+++ Laboratory work-5/main.cpp
@@ -12,137 +12,148 @@
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <fstream>
+#define Shag 1000
 
 using namespace std;
 
-vector<int> output;								//Вектор итоговой последовательности.
+vector<int> output;
 vector<int> tmp;								//Вектор возможной последовательности.
+vector<int> Lengths;
 
+//Перебирает все возможные последовательсноти.
 void processing(vector<int>& input, int i)
 {
-	int position = -1;
-
-	/*Проверяем, если получившаяся последовательность будет по длине больше
-	итоговой или равно ей, и индекс следующего числа лежит
-	за рамками массива, то перезаписываем итоговую последовательность.
-	*/
-	if (i >= input.size())
+	tmp.push_back(input[i]);
+		
+	for (int j = i + 1; j < input.size(); j++)
 	{
-		if (tmp.size() >= output.size())
+		if (tmp[tmp.size() - 1] < input[j])
 		{
-			output.resize(tmp.size());
-
-			for (int i = 0; i < tmp.size(); i++)
-			{
-				output.push_back(tmp[i]);
-			}
+			processing(input, j);
+			tmp.resize(tmp.size() - 1);
 		}
 	}
-	else
+
+	if (tmp.size() >= output.size())
 	{
-		//Сохраняем число.
-		tmp.push_back(input[i]);
+		output.resize(tmp.size());
 
-		//Рассматриваем числа со следующей позиции.
-		int j = i + 1;
-
-		//Ищем позицию числа, которое будет образовывать возрастающую последовательность с предыдущим.
-		while (j < input.size())
+		for (int i = 0; i < tmp.size(); i++)
 		{
-			if (tmp[tmp.size() - 1] < input[j])
-			{
-				position = j;
-				break;
-			}
-
-			++j;
-		}
-
-		/*Если такого числа нет, то есть мы прошлись по всему массиву,
-		то проверяем, если получившаяся последовательность будет по длине больше
-		итоговой или равно ей, то перезаписываем итоговую последовательность.
-		*/
-		if (j == input.size())
-		{
-			if (tmp.size() >= output.size())
-			{
-				output.resize(tmp.size());
-
-				for (int i = 0; i < tmp.size(); i++)
-				{
-					output[i] = tmp[i];
-				}
-
-				tmp.resize(tmp.size() - 1);
-				return;
-			}
-		}
-
-		/*Если же последовательность получилась по длине маленькая, и такой позиции
-		не нашлось, то возвращаемся, стирая элементы по дороге из временного массива.
-		*/
-		if (position == -1)
-		{
-			tmp.resize(tmp.size() - 1);
-			return;
-		}
-
-
-		//Если такая позиция нашлась, то берём это число и повторяем те же действия.
-		processing(input, position);
-
-		if (tmp.size() != 0)
-		{
-			tmp.resize(tmp.size() - 1);
-		}
-
-		/*Теперь, стирая число, которое мы сохранили до этого, берём следующее
-		после рассматриваемого и повторяем те же действия.
-		*/
-		processing(input, i + 1);
-
-		if (tmp.size() != 0)
-		{
-			tmp.resize(tmp.size() - 1);
+			output[i] = tmp[i];
 		}
 	}
 }
 
+//Считает максимальную длину возрастающей последовательности для каждого элемента в массиве.
+int processing2(vector<int>& input, int i, int k)
+{
+	int max = 1;
+	int Length = 1;
+
+	Lengths[i] = 1;
+
+	for (int j = i + 1; j < k; j++)
+	{
+		if (input[i] < input[j])
+		{
+			if (Lengths[j] != 0)
+			{
+				Length += Lengths[j];
+			}
+			else
+			{
+				Length += processing2(input, j, k);
+			}
+
+			if (max <= Length)
+			{
+				max = Length;
+				Lengths[i] = Length;
+			}
+
+			Length = 1;
+		}
+	}
+
+	return max;
+}
+
 int main()
 {
-	size_t N;							//Цисло цифр.
+	size_t N;
 
-	cin >> N;								
+	cin >> N;
 
-	vector<int> input(N);				//Исходная последовательность чисел.
+	vector<int> input(N);
+
+	srand(time(NULL));
 
 	for (size_t i = 0; i < N; i++)
 	{
-		cin >> input[i];
+		input[i] = i + 1;
 	}
 
-	/*Находим позицию числа, которое может образовать возрастающую последовательность 
-	хотя бы из двух чисел, и рассматриваем числа, начиная с этой позиции.
-	*/
-	for (size_t i = 0; i < N - 1; i++)
+	cout << endl;
+
+	chrono::time_point<chrono::high_resolution_clock> startProcessing2, endProcessing2;
+
+	for (size_t k = Shag; k <= N; k += Shag)
 	{
-		if (input[i] > input[i + 1])
+		Lengths.resize(N);
+
+		startProcessing2 = chrono::high_resolution_clock::now();
+
+		for (size_t j = 0; j < k; j++)
 		{
-			continue;
+			processing2(input, j, k);
 		}
 
-		processing(input, i);
-		break;
+		endProcessing2 = chrono::high_resolution_clock::now();
+
+		double delta = chrono::duration_cast<chrono::milliseconds> (endProcessing2 - startProcessing2).count();
+
+		cout << "----------------------------------------------------" << endl;
+		cout << k << ": " << "Processing2(): " << delta << " milliseconds" << endl;
+		cout << "----------------------------------------------------" << endl;
+
+		Lengths.clear();
+		
 	}
 
-	//Выводим число невычеркнутых чисел.
+	cout << endl;
+	cout << endl;
+
+	chrono::time_point<chrono::high_resolution_clock> startProcessing, endProcessing;
+
+	for (size_t k = Shag; k <= N; k += Shag)
+	{
+		startProcessing = chrono::high_resolution_clock::now();
+
+		for (size_t j = 0; j < k; j++)
+		{
+			processing(input, j);
+		}
+
+		endProcessing = chrono::high_resolution_clock::now();
+
+		double delta = chrono::duration_cast<chrono::milliseconds> (endProcessing - startProcessing).count();
+
+		cout << "----------------------------------------------------" << endl;
+		cout << k << ": " << "Processing(): " << delta << " milliseconds" << endl;
+		cout << "----------------------------------------------------" << endl;
+	}
+
+	/*
 	cout << output.size() << endl;
 
-	//Выводим итоговую последовательность.
 	for (size_t i = 0; i < output.size(); i++)
 	{
 		cout << output[i] << " ";
 	}
+	*/
 
 	cout << endl;
 	system("Pause");
